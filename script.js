a8ad746d2c327b54341c58479edee6b26efaf7a1
BelalHashem007 <belal.hashem.saleh@gmail.com>
2024-07-17 16:02:44 +0300

    Adding playgame function
    adding the last function "playgame" which loops 5 times over "playround" function.
    and it will determine the winner after finishing the 5 rounds.
    and with that this project is done.

--- script.js
+++ script.js
@@ -25,33 +25,45 @@ function getHumanChoice() {
         getHumanChoice();
     }
 }
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-
-    function playRound(humanChoice,computerChoice) {
-        if (humanChoice === computerChoice) {
-            console.log("Draw! human: "+ humanChoice +", computer: "+ computerChoice);
-        } else if (humanChoice === "rock" && computerChoice === "scissors") {
-            console.log("You win! human: "+ humanChoice +", computer: "+ computerChoice);
-            humanScore++;
-        } else if (humanChoice === "rock" && computerChoice === "paper") {
-            console.log("You lose! human: "+ humanChoice +", computer: "+ computerChoice);
-            computerScore++;
-        } else if (humanChoice === "paper" && computerChoice === "scissors") {
-            console.log("You lose! human: "+ humanChoice +", computer: "+ computerChoice);
-            computerScore++;
-        } else if (humanChoice === "paper" && computerChoice === "rock") {
-            console.log("You win! human: "+ humanChoice +", computer: "+ computerChoice);
-            humanScore++;
-        } else if (humanChoice === "scissors" && computerChoice === "paper") {
-            console.log("You win! human: "+ humanChoice +", computer: "+ computerChoice);
-            humanScore++;
-        } else if (humanChoice === "scissors" && computerChoice === "rock") {
-            console.log("You lose! human: "+ humanChoice +", computer: "+ computerChoice);
-            computerScore;
-        }
+    
+function playGame(){
+    for (i=0; i<=4; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        function playRound(humanChoice,computerChoice) {
+            if (humanChoice === computerChoice) {
+                console.log("Draw! human: "+ humanChoice +", computer: "+ computerChoice);
+            } else if (humanChoice === "rock" && computerChoice === "scissors") {
+                console.log("You win! human: "+ humanChoice +", computer: "+ computerChoice);
+                humanScore++;
+            } else if (humanChoice === "rock" && computerChoice === "paper") {
+                console.log("You lose! human: "+ humanChoice +", computer: "+ computerChoice);
+                computerScore++;
+            } else if (humanChoice === "paper" && computerChoice === "scissors") {
+                console.log("You lose! human: "+ humanChoice +", computer: "+ computerChoice);
+                computerScore++;
+            } else if (humanChoice === "paper" && computerChoice === "rock") {
+                console.log("You win! human: "+ humanChoice +", computer: "+ computerChoice);
+                humanScore++;
+            } else if (humanChoice === "scissors" && computerChoice === "paper") {
+                console.log("You win! human: "+ humanChoice +", computer: "+ computerChoice);
+                humanScore++;
+            } else if (humanChoice === "scissors" && computerChoice === "rock") {
+                console.log("You lose! human: "+ humanChoice +", computer: "+ computerChoice);
+                computerScore++;
+            }
     }
     playRound(humanSelection,computerSelection);
+    }
+    if (humanScore > computerScore){
+        console.log("Winner is: human.\nScore: human("+humanScore+"), computer("+computerScore+")");
+    } else if (humanScore < computerScore) {
+        console.log("Winner is: computer.\nScore: human("+humanScore+"), computer("+computerScore+")");
+    } else {
+        console.log("Draw!.\nScore: human("+humanScore+"), computer("+computerScore+")");
+    }
+}
+    
 
 
 
