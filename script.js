b09fd0d8806f15114b6488bab156dc31c19b3ae4
BelalHashem007 <belal.hashem.saleh@gmail.com>
2024-07-17 15:41:49 +0300

    Adding 2 new functions
    making 2 new functions to get the human and computer choice.

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 console.log("Hello World!");
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+    let rock = "rock";
+    let paper = "paper";
+    let scissors = "scissors";
+    let randomNum =Math.floor(Math.random()*3)+1;
+    if (randomNum === 1) {
+        return rock;
+    } else if (randomNum === 2) {
+        return paper;
+    } else {
+        return scissors;
+    }
+}
+
+function getHumanChoice() {
+    let input = prompt("Write down : 'rock', 'paper' or 'scissors' ",0);
+    if (input.toLowerCase() === "rock" || input.toLowerCase() === "paper" || input.toLowerCase() === "scissors") {
+    return input.toLowerCase();
+    } else {
+        alert("invalid input, try again.")
+        getHumanChoice();
+    }
+}
+
+
+
+
